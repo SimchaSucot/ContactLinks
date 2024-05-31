c3f01fdc02994524aed0b7310b13e8150d53ebe5
Simcha Sucot <simchasucot@gmail.com>
2024-05-31 09:59:19 +0300

    Added floating buttons on the first page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,7 @@
+// Welcome.jsx
 import React from 'react';
 import './Welcome.css'; // Assuming you have a CSS file for custom styles
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
@@ -7,7 +9,7 @@ const Welcome = () => {
       <div className="absolute inset-0 bg-logo bg-cover bg-center"></div>
       <div className="relative max-w-4xl mx-auto p-4 text-center mt-0" style={{ marginTop: '-3cm' }}>
         <h1 className="text-4xl md:text-6xl font-bold fade-in">ברוכים הבאים ל</h1>
-        <h1 className="text-4xl md:text-6xl font-bold fade-in">ContactLinks</h1>
+        <h1 className="text-4xl md:text-6xl font-bold fade-in">Contact Links</h1>
         <p className="mt-4 text-xl md:text-2xl scroll-text">
           האתר שלנו מספק דרך קלה ליצור ולשתף קישורי קשר חשובים. תוכלו לשמור את כל הקישורים שלכם במקום אחד,
           ולשתף אותם בקלות עם אחרים. המערכת מאפשרת לנהל קישורים בצורה חכמה ויעילה, עם ממשק משתמש מודרני ודינמי
@@ -16,6 +18,11 @@ const Welcome = () => {
           בין אם אתם אנשי מקצוע שרוצים לשתף קישורים ללקוחות, או פשוט משתמשים פרטיים שמחפשים דרך נוחה ליצור את קישורי הקשר שלכם
            כאן הפתרון המושלם עבורכם
         </p>
+        <div className="floating-buttons mt-8 flex-row-reverse">
+          <Link to="/email-link" className="menu-button fade-in-delay">קישור לאימייל</Link>
+          <Link to="/whatsapp-link" className="menu-button fade-in-delay">קישור לוואצאפ</Link>
+          <Link to="/phone-link" className="menu-button fade-in-delay">קישור לטלפון</Link>
+        </div>
       </div>
     </div>
   );
