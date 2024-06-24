53b5edcf820c5c2beba20924e6ca51289105174a
Simcha Sucot <simchasucot@gmail.com>
2024-06-24 19:47:21 +0300

    Handling 410 error for unavailable videos and adding detailed logging

--- server/youtubeHandler.js
+++ server/youtubeHandler.js
@@ -4,11 +4,15 @@ import path from 'path';
 
 export async function youtubeHandler(req, res) {
   const { url } = req.body;
+  console.log('Received URL:', url);
 
   try {
     const videoId = ytdl.getURLVideoID(url);
+    console.log('Video ID:', videoId);
+
     const info = await ytdl.getInfo(videoId);
     const videoFormat = ytdl.chooseFormat(info.formats, { quality: '18' });
+    console.log('Chosen format:', videoFormat);
 
     const videoSize = parseInt(videoFormat.contentLength, 10);
     const maxVideoSize = 70 * 1024 * 1024; // 70MB
@@ -27,13 +31,15 @@ export async function youtubeHandler(req, res) {
     videoStream.pipe(writeStream);
 
     writeStream.on('finish', () => {
-      // בדיקת קיום הקובץ לפני שליחת התגובה
+      console.log('Finished writing video:', videoPath);
+      
       fs.access(videoPath, fs.constants.F_OK, (err) => {
         if (err) {
           console.error('Error accessing the video file after writing:', err);
           return res.status(500).json({ message: 'Error accessing the video file after writing.' });
         }
-        // הגדרת טיימר למחיקת הקובץ לאחר 5 דקות
+        console.log('Video file accessible:', videoPath);
+
         setTimeout(() => {
           fs.unlink(videoPath, (err) => {
             if (err) {
@@ -45,12 +51,12 @@ export async function youtubeHandler(req, res) {
         }, 3 * 60 * 1000); // 3 דקות
 
         res.json({
-          videoId, // הוספת videoId לתשובה
+          videoId,
           title,
           uploadDate,
           viewCount,
           videoPath: `/videos/${videoId}.mp4`,
-          message: `הסרטון הוכן בהצלחה (${(videoSize / 1048576).toFixed(1)}MB). הקובץ יימחק לאחר 5 דקות.`
+          message: `הסרטון הוכן בהצלחה (${(videoSize / 1048576).toFixed(1)}MB). הקובץ יימחק לאחר 3 דקות.`
         });
       });
     });
@@ -61,6 +67,10 @@ export async function youtubeHandler(req, res) {
     });
 
   } catch (error) {
+    if (error.statusCode === 410) {
+      console.error('Video is no longer available:', error);
+      return res.status(410).json({ message: 'הסרטון כבר לא זמין. אנא נסה סרטון אחר.' });
+    }
     console.error('Error processing the video:', error);
     res.status(500).json({ message: 'הייתה בעיה בעיבוד הבקשה שלך. אנא נסה שוב מאוחר יותר.' });
   }
